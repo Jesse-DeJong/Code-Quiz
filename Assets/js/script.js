f1b4b05e4510572ae949df8e3bfbdd9d4bbb001d
Jesse De Jong <jdoceau@gmail.com>
2021-06-21 18:30:44 +1000

    fixed js Index.html check to index.html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -231,7 +231,7 @@ function mainMenu () {
 
 
 function init() {
-    if (window.location.href.includes("Index.html") == true) {      // Run only when on Index.html
+    if (window.location.href.includes("index.html") == true) {      // Run only when on Index.html
         mainMenu();                                                 // Call function to build elements and inject content for the starting position
     } else if (window.location.href.includes("Highscores.html") == true) {
         for (i = 0; i < hs.length; i++) {                                                            // Loop to create a new <li> for each item in HS array
